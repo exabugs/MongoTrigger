23f3033eba0d112b9e38c82b1e5b717610fe1242
exabugs <exabugs@gmail.com>
2014-06-16 14:11:42 +0900

    First Commit.

---
+++ trigger.js
@@ -0,0 +1,73 @@
+var meta_collection = 'metainfo'; // collection:metainfo
+var stop_collection = 'test.stop'; // database:test collection:stop
+
+function copy( op, tag, infos ) {
+	var conn = connect( tag[0] );
+	for ( var i = 0; i < infos.length; i++ ) {
+		var info = infos[i];
+		if ( info.master.collection != tag[1] ) continue;
+		var master = get_master( op.o, info );
+		if ( !master ) continue;
+		var referrer = get_referrer( op.o2, info );
+		if ( !referrer ) continue;
+		conn[ info.referrer.collection ].update( referrer, { $set: master }, { multi: true } );
+	}
+}
+
+function get_master( data, info ) {
+	var obj = {};
+	var fields = info.master.fields;
+	var referrer_field = info.referrer.multi ? [info.referrer.field, '$'].join('.') : info.referrer.field;
+	var update = false;
+	for ( var i = 0; i < fields.length; i++ ) {
+		var field = fields[i];
+		var o = data['$set'] || data;
+		if ( !o[field] ) continue;
+		obj[ [referrer_field, field].join('.') ] = o[field];
+		update = true;
+	}
+	return update ? obj : null;
+}
+
+function get_referrer( data, info ) {
+	if ( !data._id ) return null;
+	var obj = {};
+	obj[ [info.referrer.field, '_id'].join('.') ] = data._id;
+	return obj;
+}
+
+
+var option = DBQuery.Option.awaitData | DBQuery.Option.tailable;
+var cursor = connect( 'local' ).oplog.rs.find().addOption( option );
+
+while ( cursor.hasNext() ) { cursor.next(); } // skip
+
+var infos = {};
+var stop = false;
+while ( !stop ) {
+	var now = new Date();
+//	printjson( now );
+
+	while ( cursor.hasNext() ) {
+		var op = cursor.next();
+printjson( op );
+		if ( op.ns === stop_collection ) {
+			stop = true;
+			break;
+		}
+
+		var tag = op.ns.split('.');
+		if ( !infos[ tag[0] ] || tag[1] === meta_collection ) {
+			var conn = connect( tag[0] );
+			infos[ tag[0] ] = conn[meta_collection].find(  ).toArray();
+		}
+
+		if ( !op.o2 ) continue;
+		copy( op, tag, infos[ tag[0] ] );
+	}
+
+	// Safety Trap for busy loop.
+	if ( (new Date()) - now < 100 ) {
+		break;
+	}
+}
