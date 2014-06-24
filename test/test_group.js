9ae4bc53c344724aaa85a3c8c039d8c2992d4ab6
exabugs <exabugs@gmail.com>
2014-06-24 23:43:56 +0900

    Ancestors & Embeddeds test succeeded.

--- test/test_group.js
+++ test/test_group.js
@@ -7,9 +7,7 @@
 print("------- Case 1 ancestors -------");
 
 // Model Tree Structures with an Array of Ancestors
-
 // http://docs.mongodb.org/manual/tutorial/model-tree-structures-with-ancestors-array/
-
 // When modified parent, as soon as possible maintain ancestors myself and others.
 
 db.metadata.ancestors.drop();
@@ -29,6 +27,12 @@ db.metadata.ancestors.insert({collection: 'categories', parent: 'parent', ancest
 db.categories.update( {_id: "MongoDB"}, {$set: {parent: "Languages"}});
 db.categories.update( {_id: "dbm"}, {$set: {parent: "MongoDB"}});
 
+sleep(100);
+
+var Obj_0 = db.categories.findOne( {_id: "dbm"}, {ancestors: 1} );
+var Obj_1 = { "_id" : "dbm", "ancestors" : [ "Books", "Programming", "Language", "MongoDB", "dbm" ] };
+assert.eq.automsg(Obj_0, Obj_1);
+
 
 
 // Case 2 ancestors
@@ -47,4 +51,48 @@ db.categories.insert( { _id: "MongoDB",     parent: "Databases"   } );
 db.categories.insert( { _id: "dbm",         parent: "Databases"   } );
 
 db.categories.update( {_id: "MongoDB"}, {$set: {parent: "Languages"}});
+sleep(100);
+var Obj_20 = db.categories.findOne( {_id: "MongoDB"}, {ancestors: 1} );
+var Obj_21 = { "_id" : "MongoDB", "ancestors" : [ "Books", "Programming", "Languages", "MongoDB" ] }
+assert.eq.automsg(Obj_20, Obj_21);
+
 db.categories.update( {_id: "dbm"}, {$set: {parent: "MongoDB"}});
+sleep(100);
+var Obj_30 = db.categories.findOne( {_id: "dbm"}, {ancestors: 1} );
+var Obj_31 = { "_id" : "dbm", "ancestors" : [ "Books", "Programming", "Languages", "MongoDB", "dbm" ] }
+assert.eq.automsg(Obj_30, Obj_31);
+
+
+
+
+// Case 3 ancestors
+// embeddeds との併用
+print("------- Case 3 ancestors & embeddeds -------");
+
+db.metadata.ancestors.drop();
+db.metadata.ancestors.insert({collection: 'categories', parent: 'parent', ancestors: 'ancestors'});
+db.metadata.embeddeds.drop();
+db.metadata.embeddeds.insert({referrer: {collection: 'books', field: 'categories', multi: true}, master: {collection: 'categories', fields: ['ancestors']}});
+
+
+db.categories.drop();
+db.categories.insert( { _id: "Books",       parent: null          } );
+db.categories.insert( { _id: "Programming", parent: "Books"       } );
+db.categories.insert( { _id: "Databases",   parent: "Programming" } );
+db.categories.insert( { _id: "Languages",   parent: "Programming" } );
+db.categories.insert( { _id: "MongoDB",     parent: "Databases"   } );
+db.categories.insert( { _id: "dbm",         parent: "Databases"   } );
+
+db.books.drop();
+db.books.insert( {_id: "A", categories: [{_id: "MongoDB"}]} );
+
+
+db.categories.update( {_id: "MongoDB"}, {$set: {parent: "Languages"}});
+sleep(100);
+var Obj_40 = db.categories.findOne( {_id: "MongoDB"}, {ancestors: 1} );
+var Obj_41 = { "_id" : "MongoDB", "ancestors" : [ "Books", "Programming", "Languages", "MongoDB" ] }
+assert.eq.automsg(Obj_20, Obj_21);
+
+var Obj_50 = db.books.findOne( {_id: "A"}, {categories: 1} );
+var Obj_51 = { "_id" : "A", "categories" : [ { "_id" : "MongoDB", "ancestors" : [ "Books", "Programming", "Languages", "MongoDB" ] } ] };
+assert.eq.automsg(Obj_50, Obj_51);
