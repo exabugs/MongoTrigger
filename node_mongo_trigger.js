0a03750691b0a84207127c5c7779302138a67274
exabugs <exabugs@gmail.com>
2014-07-14 10:11:48 +0900

    Fixed node_mongo_trigger.js.

--- node_mongo_trigger.js
+++ node_mongo_trigger.js
@@ -1,260 +1,357 @@
 "use strict";
 
 var async = require('async');
+var events = require('events');
+var stream = require('stream');
+var util = require('util');
 var MongoClient = require('mongodb').MongoClient;
 
 var direct_port = 27017;
 var mongos_port = 27017;
 
-var dbs = ['test'];
+var dbs = ['test', 'test1', 'test2'];
 
 var metadata = 'metadata';
 
 // Trigger Definition
 var trigger_data = {};
 var trigger_func = {
-//	embeddeds: do_embeddeds,
-//	ancestors: do_ancestors
+  ancestors: do_ancestors,
+  embeddeds: do_embeddeds
 };
 
 
-
-var now = Math.ceil( (new Date()).getTime() / 1000 );
+var now = Math.ceil((new Date()).getTime() / 1000);
 
 var connections = {};
 
 var tasks = [];
 
-tasks.push( function( next ) {
-	async.each(dbs, function( name, done ) {
-		var local = url( '127.0.0.1', mongos_port, name );
-		MongoClient.connect( local, function ( err, db ) {
-			for ( var key in trigger_func ) {
-				var coll = [metadata, key].join('.');
-				trigger_data[ name ][ key ] = db.collection( coll ).find().toArray();
-				done( err );
-			}
-		});
-	}, function ( err ) {
-		next( err );
-	});
+tasks.push(function (next) {
+  async.eachSeries(dbs, function (name, done) {
+    trigger_data[ name ] = trigger_data[ name ] || {};
+    var config = url('127.0.0.1', mongos_port, name);
+    MongoClient.connect(config, function (err, db) {
+      connections[ name ] = db;
+      getTriggerData(name, function (err) {
+        done(err)
+      });
+    });
+  }, function (err) {
+    next(err);
+  });
 });
 
-tasks.push( function( next ) {
-//			trigger_data[ tag[0] ] = trigger_data[ tag[0] ] || {};
-//			if ( tag[1] === metadata && tag[2] ) {
-//				var conn = connect( tag[0] );
-//				var collection = op.ns.slice( op.ns.indexOf('.') + 1 );
-//				trigger_data[ tag[0] ][ tag[2] ] = conn[collection].find().toArray();
-//			}
+/**
+ *
+ * @param name DB名称
+ * @param callback
+ */
+function getTriggerData(name, callback) {
+  async.eachSeries(Object.keys(trigger_func), function (key, done) {
+    var coll = [metadata, key].join('.');
+    connections[ name ].collection(coll).find().toArray(function (err, docs) {
+      trigger_data[ name ][ key ] = docs;
+      done(err);
+    });
+  }, function (err) {
+    callback(err);
+  });
+}
 
-	next(null);
-});
-
-tasks.push( function( next ) {
-	var local = url( '127.0.0.1', direct_port, 'local' );
-	MongoClient.connect( local, function ( err, conn ) {
-		next( err, conn );
-	});
-});
-
-tasks.push( function( conn, next ) {
-	var cursor = conn.collection( 'oplog.rs' ).find( {}, { tailable: true } );
-	cursor.each( function( err, op ) {
-		if ( now < op.ts.high_ ) {
-console.log(op);
-			var tag = op.ns.split('.');
-			for ( var key in trigger_func ) {
-				var data = trigger_data[ tag[0] ][ key ];
-				if ( !data ) continue; // null, undefined, empty array
-//				trigger_func[ key ]( op, tag, data );
-			}
-		}
-	});
-});
-
-async.waterfall( tasks, function( err ) {
-
+tasks.push(function (next) {
+  var config = url('127.0.0.1', direct_port, 'local');
+  MongoClient.connect(config, function (err, conn) {
+    next(err, conn);
+  });
 });
 
 
-function url( ip, port, db ) {
-	return 'mongodb://' + ip + ':' + port + '/' + db;
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function FilterBase() {
 }
 
-/*
+util.inherits(FilterBase, stream.Stream);
 
-// MongoDB Extention
-//  * Embeddeds Pattern
-//  * Ancestors Pattern
-
-var metadata = 'metadata';
-var metadata_embeddeds = 'embeddeds'; // metadata.embeddeds
-var metadata_ancestors = 'ancestors'; // metadata.ancestors
-
-var stop_collection = 'test.stop'; // database:test collection:stop
-
-// for Sharding Environment.
-function connect( name ) {
-	return db.getSisterDB( name );
-}
-
-//////////////////////////////////////////////////////
-
-function do_ancestors( op, tag, infos ) {
-	var done = false;
-	for ( var i = 0; i < infos.length; i++ ) {
-		var info = infos[i];
-		if ( info.collection != tag[1] ) continue;
-		update_ancestors( tag[0], op, info );
-		done = true;
-	}
-	return done;
-}
-
-function update_ancestors( db, op, info ) {
-	var field = info.parent; 
-	var o = op.o['$set'] || op.o;
-	if ( !o[field] ) return;
-
-	var conn = connect( db );
-	var collection = conn[ info.collection ];
-	var select = {};
-	select[ info.ancestors ] = 1;
-
-	var _id = op.o2 ? op.o2._id : o._id;
-
-	var parent_ancestors = get_ancestors( conn, info, select, o[field] );
-	var myself_ancestors = get_ancestors( conn, info, select, _id );
-	var length = myself_ancestors.length - 1;
-
-	var condition = {}; 
-	condition[ info.ancestors ] = { $in: [ _id ] };
-	var cursor = collection.find( condition, select );
-	while ( cursor.hasNext() ) {
-		var object = cursor.next();
-		var ancestors = object.ancestors || [];
-		ancestors = parent_ancestors.concat( ancestors.slice( length ) );
-		update( collection, object._id, info.ancestors, ancestors );
-	}
-}
-
-function get_ancestors( conn, info, fields, _id ) {
-	if ( !_id ) return [];
-	fields = fields || {};
-	fields[ info.ancestors ] = 1;
-	var collection = conn[ info.collection ];
-	var object = collection.findOne( { _id: _id }, fields );
-	if ( !object ) return [];
-	var ancestors = object[ info.ancestors ];
-	if ( !ancestors ) {
-		var parent_ancestors = get_ancestors( conn, info, object[ info.parent ], fields );
-		ancestors = parent_ancestors.concat( object._id );
-		update( collection, object._id, info.ancestors, ancestors );
-	}
-	return ancestors;
-}
-
-function update( collection, _id, key, value ) {
-	var object = {};
-	object[ key ] = value;
-	collection.update( { _id: _id }, { $set: object } );
-}
-
-//////////////////////////////////////////////////////
-
-function do_embeddeds( op, tag, infos ) {
-	var done = false;
-	if ( op.o2 === undefined ) return done;
-	for ( var i = 0; i < infos.length; i++ ) {
-		var info = infos[i];
-		if ( info.master.collection != tag[1] ) continue;
-		var master = get_master( op.o, info );
-		if ( !master ) continue;
-		var referrer = get_referrer( op.o2, info );
-		if ( !referrer ) continue;
-		var conn = connect( info.referrer.db || tag[0] );
-		conn[ info.referrer.collection ].update( referrer, { $set: master }, { multi: true } );
-		done = true;
-	}
-	return done;
-}
-
-function get_master( data, info ) {
-	var obj = {};
-	var fields = info.master.fields;
-	var referrer_field = info.referrer.multi ? [info.referrer.field, '$'].join('.') : info.referrer.field;
-	var update = false;
-	for ( var i = 0; i < fields.length; i++ ) {
-		var field = fields[i];
-		var o = data['$set'] || data;
-		if ( !o[field] ) continue;
-		obj[ [referrer_field, field].join('.') ] = o[field];
-		update = true;
-	}
-	return update ? obj : null;
-}
-
-function get_referrer( data, info ) {
-	if ( !data._id ) return null;
-	var obj = info.referrer.condition ? info.referrer.condition : {};
-	obj[ [info.referrer.field, '_id'].join('.') ] = data._id;
-	return obj;
-}
-
-//////////////////////////////////////////////////////
-
-// Trigger Definition
-var trigger_data = {};
-var trigger_func = {
-	embeddeds: do_embeddeds,
-	ancestors: do_ancestors
+FilterBase.prototype.end = function () {
+  this.emit('close');
 };
 
-var option = DBQuery.Option.awaitData | DBQuery.Option.tailable;
-var cursor = connect( 'local' ).oplog.rs.find().addOption( option );
+FilterBase.prototype.pipe = function (dest) {
+  this.piped = true;
+  stream.Stream.prototype.pipe.apply(this, arguments);
+  return dest;
+};
 
-for ( var stop = false, cursor = cursor.skip( cursor.count() ); !stop; ) {
-	var now = new Date();
-//printjson( now );
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	while ( cursor.hasNext() ) {
-		var op = cursor.next();
-//printjson( op );
-		if ( op.ns === stop_collection ) {
-			stop = true;
-			break;
-		}
-
-		// Primary Server Only.
-		if ( rs.isMaster().primary != rs.isMaster().me ) {
-			continue;
-		}
-
-		var tag = op.ns.split('.');
-		trigger_data[ tag[0] ] = trigger_data[ tag[0] ] || {};
-		if ( tag[1] === metadata && tag[2] ) {
-			var conn = connect( tag[0] );
-			var collection = op.ns.slice( op.ns.indexOf('.') + 1 );
-			trigger_data[ tag[0] ][ tag[2] ] = conn[collection].find().toArray();
-		}
-
-		for ( var key in trigger_func ) {
-			var data = trigger_data[ tag[0] ][ key ];
-			if ( !data ) continue; // null, undefined, empty array
-
-			var start = new Date();
-			var done = trigger_func[ key ]( op, tag, data );
-			if ( done ) {
-				var time = (new Date()) - start;
-				printjson( { trigger: key, time: time, collection: op.ns } );
-			}
-		}
-	}
-
-	// Safety Trap for busy loop.
-	if ( (new Date()) - now < 100 ) {
-		break;
-	}
+function Filter0(now) {
+  this.now = now;
+  this.piped = false;
+  this.writable = true;
+  this.timeoutId = 0;
+  this.ops = [];
 }
-*/
+
+util.inherits(Filter0, FilterBase);
+
+Filter0.prototype.write = function (op) {
+
+  if (this.now < op.ts.high_) {
+
+    var tag = op.ns.split('.');
+    if (tag[1] !== '$cmd') {
+
+      this.ops.push(op);
+
+      if (this.timeoutId) {
+        clearTimeout(this.timeoutId)
+      }
+
+      var self = this;
+      this.timeoutId = setTimeout(function () {
+        self.timeoutId = 0;
+        self.emit('data', self.ops);
+        self.ops = [];
+      }, 200);
+    }
+  }
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function Filter1() {
+  this.piped = false;
+  this.writable = true;
+}
+
+util.inherits(Filter1, FilterBase);
+
+Filter1.prototype.write = function (ops) {
+
+  var self = this;
+
+  async.eachSeries(ops, function (op, next) {
+
+    var tag = op.ns.split('.');
+    trigger_data[ tag[0] ] = trigger_data[ tag[0] ] || {};
+
+    if (tag[1] === metadata && tag[2]) {
+      // 定義が変更されていれば再読み込み
+      getTriggerData(tag[0], function (err) {
+        next();
+      });
+    } else {
+      next();
+    }
+
+  }, function (err) {
+    self.emit('data', ops);
+  });
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function StemFilter() {
+  this.piped = false;
+  this.writable = true;
+}
+
+util.inherits(StemFilter, FilterBase);
+
+StemFilter.prototype.write = function (ops) {
+  var self = this;
+
+  async.eachSeries(ops, function (op, next) {
+
+    var tag = op.ns.split('.');
+    async.eachSeries(Object.keys(trigger_func), function (key, done) {
+      if (trigger_data[ tag[0] ]) {
+        var data = trigger_data[ tag[0] ][ key ];
+        if (data) { // null, undefined, empty array
+          trigger_func[ key ](op, tag, data, function (err) {
+            done(err);
+          });
+        } else {
+          done(null);
+        }
+      } else {
+        done(null);
+      }
+    }, function (err) {
+      next();
+    });
+
+  }, function (err) {
+    self.emit('data', ops);
+  });
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+tasks.push(function (conn, next) {
+
+  var stream = conn.collection('oplog.rs').find({}, { tailable: true }).stream();
+
+  var filter0 = new Filter0(now);
+  var filter1 = new Filter1();
+  var filter = new StemFilter();
+
+  stream.pipe(filter0).pipe(filter1).pipe(filter);
+
+});
+
+
+async.waterfall(tasks, function (err) {
+
+});
+
+function url(ip, port, db) {
+  return 'mongodb://' + ip + ':' + port + '/' + db;
+}
+
+//////////////////////////////////////////////////////
+
+function do_ancestors(op, tag, infos, callback) {
+  var done = false;
+  async.eachSeries(infos, function (info, next) {
+    if (info.collection === tag[1]) {
+      update_ancestors(tag[0], op, info, function (err) {
+        done = true;
+        next(err);
+      });
+    } else {
+      next(null);
+    }
+  }, function (err) {
+    return callback(err, done);
+  });
+}
+
+function update_ancestors(db, op, info, callback) {
+  var field = info.parent;
+  if (op.op === 'i' && op.o[ info.ancestors ]) {
+    return callback(null);
+  }
+  var o = op.o['$set'] || op.o;
+  if (!o[field]) {
+    return callback(null);
+  }
+
+  var conn = connections[db];
+  var collection = conn.collection(info.collection);
+  var select = {};
+  select[ info.ancestors ] = 1;
+
+  var _id = op.o2 ? op.o2._id : o._id;
+
+  get_ancestors(conn, info, select, o[field], function (err, parent_ancestors) {
+    get_ancestors(conn, info, select, _id, function (err, myself_ancestors) {
+      var length = myself_ancestors.length - 1;
+      var condition = {};
+      condition[ info.ancestors ] = { $in: [ _id ] };
+      collection.find(condition, select).toArray(function (err, objects) {
+        async.eachSeries(objects, function (object, next) {
+          var ancestors = object.ancestors || [];
+          ancestors = parent_ancestors.concat(ancestors.slice(length));
+          update(collection, object._id, info.ancestors, ancestors, function (err) {
+            next(err);
+          });
+        }, function (err) {
+          callback(err);
+        });
+      });
+    });
+  });
+}
+
+function get_ancestors(conn, info, fields, _id, callback) {
+  if (!_id) {
+    return callback(null, []);
+  } else {
+    fields = fields || {};
+    fields[ info.ancestors ] = 1;
+    var collection = conn.collection(info.collection);
+    collection.findOne({ _id: _id }, fields, function (err, object) {
+      if (object) {
+        var ancestors = object[ info.ancestors ];
+        if (!ancestors) {
+          get_ancestors(conn, info, fields, object[ info.parent ], function (err, parent_ancestors) {
+            ancestors = parent_ancestors.concat(object._id);
+            return update(collection, object._id, info.ancestors, ancestors, function (err) {
+              callback(err, ancestors);
+            });
+          });
+        } else {
+          return callback(null, ancestors);
+        }
+      } else {
+        return callback(null, []);
+      }
+    });
+  }
+}
+
+function update(collection, _id, key, value, callback) {
+  var object = {};
+  object[ key ] = value;
+  collection.update({ _id: _id }, { $set: object }, function (err, result) {
+    return callback(err, result);
+  });
+}
+
+//////////////////////////////////////////////////////
+
+function do_embeddeds(op, tag, infos, callback) {
+  var done = false;
+  if (op.o2 === undefined) {
+    return callback(null, done);
+  } else {
+    async.eachSeries(infos, function (info, next) {
+      if (info.master.collection === tag[1]) {
+        var master = get_master(op.o, info);
+        if (master) {
+          var referrer = get_referrer(op.o2, info);
+          if (referrer) {
+            var conn = connections[info.referrer.db || tag[0]];
+            conn.collection(info.referrer.collection).update(referrer, { $set: master }, { multi: true }, function (err, result) {
+              done = true;
+              next(null);
+            });
+          } else {
+            next(null);
+          }
+        } else {
+          next(null);
+        }
+      } else {
+        next(null);
+      }
+    }, function (err) {
+      callback(err, done);
+    });
+  }
+}
+
+function get_master(data, info) {
+  var obj = {};
+  var fields = info.master.fields;
+  var referrer_field = info.referrer.multi ? [info.referrer.field, '$'].join('.') : info.referrer.field;
+  var update = false;
+  for (var i = 0; i < fields.length; i++) {
+    var field = fields[i];
+    var o = data['$set'] || data;
+    if (!o[field]) continue;
+    obj[ [referrer_field, field].join('.') ] = o[field];
+    update = true;
+  }
+  return update ? obj : null;
+}
+
+function get_referrer(data, info) {
+  if (!data._id) return null;
+  var obj = info.referrer.condition ? info.referrer.condition : {};
+  obj[ [info.referrer.field, '_id'].join('.') ] = data._id;
+  return obj;
+}
