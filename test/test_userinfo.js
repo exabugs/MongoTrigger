46c87d9fd14a109bc743a9e3a880db77c028faf7
exabugs <exabugs@gmail.com>
2014-08-11 10:59:46 +0900

    userinfo同期の集計機能についてのテスト追加

--- test/test_userinfo.js
+++ test/test_userinfo.js
@@ -13,7 +13,7 @@ db.test_additions.insert({"messageId" : "534263c5641ab7e4020ec20b", "read" : "1"
 
 sleep(400);
 
-var message0 = db.test_messages.findOne({"_id" : ObjectId("534263c5641ab7e4020ec20b")});
+var message0 = db.test_messages.findOne({"_id" : ObjectId("534263c5641ab7e4020ec20b")}, {'userinfo_reads':1});
 var message1 = { "_id" : ObjectId("534263c5641ab7e4020ec20b"), "userinfo_reads" : [ { "_id" : ObjectId("533e710586ab971418ac1025"), "v" : true } ] };
 assert.eq.automsg(message0, message1);
 
@@ -35,7 +35,7 @@ db.test_additions.insert({"messageId" : "534263c5641ab7e4020ec20b", "read" : "0"
 
 sleep(400);
 
-var message0 = db.test_messages.findOne({"_id" : ObjectId("534263c5641ab7e4020ec20b")});
+var message0 = db.test_messages.findOne({"_id" : ObjectId("534263c5641ab7e4020ec20b")}, {'userinfo_reads':1});
 var message1 = { "_id" : ObjectId("534263c5641ab7e4020ec20b"), "userinfo_reads" : [ { "_id" : ObjectId("533e710586ab971418ac1025"), "v" : false } ] };
 assert.eq.automsg(message0, message1);
 
@@ -61,6 +61,32 @@ db.test_additions.update({"messageId" : "534263c5641ab7e4020ec20b", "uid" : '533
 
 sleep(400);
 
-var message0 = db.test_messages.findOne({"_id" : ObjectId("534263c5641ab7e4020ec20b")});
+var message0 = db.test_messages.findOne({"_id" : ObjectId("534263c5641ab7e4020ec20b")}, {'userinfo_reads':1});
 var message1 = { "_id" : ObjectId("534263c5641ab7e4020ec20b"), "userinfo_reads" : [ { "_id" : ObjectId("533e710586ab971418ac1025"), "v" : true } ] };
+assert.eq.automsg(message0, message1);
+
+
+
+
+// Case 4 : 集計機能
+print("------- Case 4 -------");
+
+db.metadata.userinfos.drop();
+db.metadata.userinfos.insert({referrer: {db: 'test', collection: 'test_messages', field: 'userinfo_reads'},
+  master: {collection: 'test_additions', fields: {target: 'messageId', user: 'uid', value: 'read'}, map: {'1': true, '0': false}, default: false}});
+sleep(300);
+
+db.test_messages.drop();
+db.test_messages.insert({"_id" : ObjectId("534263c5641ab7e4020ec20b")});
+db.test_additions.drop();
+db.test_additions.insert({"messageId" : "534263c5641ab7e4020ec20b", "read" : "1", "uid" : '533e710586ab971418ac1025' });
+db.test_additions.insert({"messageId" : "534263c5641ab7e4020ec20b", "read" : "1", "uid" : '533e710586ab971418ac1026' });
+db.test_additions.insert({"messageId" : "534263c5641ab7e4020ec20b", "read" : "0", "uid" : '533e710586ab971418ac1027' });
+db.test_additions.insert({"messageId" : "534263c5641ab7e4020ec20b", "read" : "2", "uid" : '533e710586ab971418ac1028' });
+db.test_additions.insert({"messageId" : "534263c5641ab7e4020ec20b", "read" : "3", "uid" : '533e710586ab971418ac1029' });
+
+sleep(400);
+
+var message0 = db.test_messages.findOne({"_id" : ObjectId("534263c5641ab7e4020ec20b")}, {'userinfo_reads_total':1});
+var message1 = { "_id" : ObjectId("534263c5641ab7e4020ec20b"), "userinfo_reads_total" : [ {"_id" : false, "value" : 3 }, { "_id" : true, "value" : 2 } ] };
 assert.eq.automsg(message0, message1);
